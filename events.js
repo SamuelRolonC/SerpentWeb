9a5877525b0d4f6cc6649be1003b91382395157c
Samuel Rolon Cicciari <samuelrolonc@gmail.com>
2018-02-16 18:03:32 -0300

    añadiendo dibujar con mouse

--- events.js
+++ events.js
@@ -3,30 +3,17 @@
 console.log("events.js enlazado")
 
 
-// @Section Class statements
+// @Section Class and variables declarations
 
-class Animal {
-  constructor(x ,y, url = "") {
-    this.x = x
-    this.y = y
-    this.url = url
-    this.ready = false
-  }
-}
+var square = document.getElementById("drawArea")
+var paper = square.getContext("2d")
+var clickState = false
 
-var keys = {
-  UP: 38,
-  DOWN: 40,
-  LEFT: 37,
-  RIGHT: 39
-};
+var brushx,brushy
+var lineColor = "red"
 
 
 // @Section Function statements
-
-/*function myRandom(min, max) {
-  return Math.round(Math.random()*(max-min+1)+10)
-}*/
 
 function drawLine(canvas, x1, y1, x2, y2, color = "black", width = 3) {
   canvas.beginPath()
@@ -38,39 +25,30 @@ function drawLine(canvas, x1, y1, x2, y2, color = "black", width = 3) {
   canvas.closePath()
 }
 
-function keyboardDraw(e) {
-  let lineColor = "red"
-  let movement = 5
-
-  switch (e.keyCode) {
-    case keys.UP:
-      drawLine(paper, x, y, x, y - movement, lineColor)
-      y -= movement
-      break;
-    case keys.DOWN:
-      drawLine(paper, x, y, x, y + movement, lineColor)
-      y += movement
-      break;
-    case keys.LEFT:
-      drawLine(paper, x, y, x - movement, y, lineColor)
-      x -= movement
-      break;
-    case keys.RIGHT:
-      drawLine(paper, x, y, x + movement, y, lineColor)
-      x += movement
-      break;
+function mouseDraw(e) {
+  if (clickState) {
+    console.log(e.x + "," + e.y)
+    drawLine(paper, brushx, brushy, e.x, e.y)
+    brushx = e.x
+    brushy = e.y
   }
+}
+
+function mouseActivate(e) {
+  console.log("Mouse bottom active")
+  clickState = true
+  brushx = e.x
+  brushy = e.y
+}
+
+function mouseDectivate(e) {
+  console.log("Mouse bottom deactive")
+  clickState = false
 }
 
 
 // @Section Body
 
-document.addEventListener("keydown", keyboardDraw)
-
-var square = document.getElementById("drawArea")
-var paper = square.getContext("2d")
-
-var x = 150
-var y = 150
-
-drawLine(paper, 149, 149, 151, 151, 200)
+drawArea.addEventListener("mousedown", mouseActivate)
+drawArea.addEventListener("mouseup", mouseDectivate)
+drawArea.addEventListener("mousemove", mouseDraw)
